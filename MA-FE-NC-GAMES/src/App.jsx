690ea49002f75dc7712eae1c46d8f8b74a787aa1
Martin Abbott <abbott01@googlemail.com>
2023-05-30 09:49:13 +0100

    Carried out react app initial setup

---
+++ MA-FE-NC-GAMES/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<>
+			<h1>MA-FE-NC-GAMES</h1>
+			<p>This will be the home page!</p>
+		</>
+	);
+}
+
+export default App;
